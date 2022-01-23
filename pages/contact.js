38150b51e3c84d5f8884c320932ff6c561d9dd4e
kreyson999 <mlynekjakub05@gmail.com>
2022-01-23 22:06:07 +0100

    added validation to contact form

--- pages/contact.js
+++ pages/contact.js
@@ -1,5 +1,103 @@
+import { useState } from "react";
 import { FrequentlyAskedQuestions, SocialMediaLink } from "../components";
 import { getQuestions, REVALIDATE_PAGE_CONTENT } from '../lib/graphCMS'
+
+function validateEmail(mail) 
+{
+  if (mail.match(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/)) {
+    console.log('email jest poprawny')
+    return true
+  } else {
+    return false
+  }  
+}
+
+const ContactForm = () => {
+  const [error, setError] = useState({name: false, email: false, message: false })
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [message, setMessage] = useState('')
+
+  const handleOnChangeInput = (e) => {
+    switch(e.target.id) {
+      case 'name':
+        setName(e.target.value)
+        break
+      case 'email':
+        setEmail(e.target.value)
+        break
+      case 'message':
+        setMessage(e.target.value)
+        break
+    }
+  }
+
+  const handleSubmitingForm = (e) => {
+    if (name.length <= 4) {
+      e.preventDefault();
+      setError(state => ({...state, name: true}))
+    } else {
+      setError(state => ({...state, name: false}))
+    }
+    if (!validateEmail(email)) {
+      e.preventDefault();
+      setError(state => ({...state, email: true}))
+    } else {
+      setError(state => ({...state, email: false}))
+    }
+    if (message.length <= 12) {
+      e.preventDefault();
+      setError(state => ({...state, message: true}))
+    } else {
+      setError(state => ({...state, message: false}))
+    }
+  }
+
+  return (
+    <form target="_blank" action="https://formsubmit.co/0c7c727f618c8174184105d0aa8e722c" method="POST" className="flex flex-col">
+      <label className="text-base text-white mb-1" htmlFor="name">Twoje imię</label>
+      <input 
+      className="px-2 py-2 focus:outline-2 focus:outline focus:outline-blue-light rounded-lg text-blue-dark border-2 border-white" 
+      autoComplete="off" 
+      placeholder="Twoje imię" 
+      type="text" 
+      id="name"
+      name="name"
+      required
+      value={name}
+      onChange={handleOnChangeInput}
+      />
+      <p className="mb-5 text-red-400 text-sm">{error.name && 'Imię musi składać się z co najmniej 4 znaków!'}</p>
+      <label className="text-base text-white mb-1" htmlFor="email">Adres e-mail</label>
+      <input 
+      className="px-2 py-2 focus:outline-2 focus:outline focus:outline-blue-light rounded-lg text-blue-dark border-2 border-white" 
+      autoComplete="off" 
+      placeholder="Adres e-mail" 
+      type={'email'} 
+      id="email"
+      required
+      value={email}
+      onChange={handleOnChangeInput}
+      />
+      <p className="mb-5 text-red-400 text-sm">{error.email && 'Proszę podać prawidłowy adres e-mail!'}</p>
+      <label className="text-base text-white mb-1" htmlFor="message">Wiadomość</label>
+      <textarea 
+      className="mb-1 px-2 py-2 focus:outline-2 focus:outline focus:outline-blue-light rounded-lg text-blue-dark border-2 border-white" 
+      id="message" 
+      cols="30" 
+      placeholder="Wiadomość"
+      rows="10"
+      required
+      value={message}
+      onChange={handleOnChangeInput}
+      ></textarea>
+      <p className="mb-5 text-red-400 text-sm">{error.message && 'Wiadomość musi być dłuższa niż 12 znaków!'}</p>
+      <div className="w-full flex justify-center">
+        <button onClick={handleSubmitingForm} type={'submit'} className="bg-blue-light px-6 xl:px-8 py-1.5 xl:py-2 rounded-lg w-fit font-semibold text-white text-lg">Wyślij</button>
+      </div>
+    </form>
+  )
+}
 
 export default function Contact({ questions }) {
   return (
@@ -35,22 +133,7 @@ export default function Contact({ questions }) {
           />
         </div>
         <div className="bg-blue-dark py-8  px-4 md:px-8 rounded-xl col-span-2">
-          <form target="_blank" action="https://formsubmit.co/contact@mlynek.xyz" method="POST" className="flex flex-col">
-            <label className="text-base text-white mb-1" htmlFor="name">Twoje imię</label>
-            <input className="mb-5 px-2 py-2 focus:outline-2 focus:outline focus:outline-blue-light rounded-lg text-blue-dark border-2 border-white" autoComplete="off" placeholder="Twoje imię" type="text" id="name"/>
-            <label className="text-base text-white mb-1" htmlFor="email">Adres e-mail</label>
-            <input className="mb-5 px-2 py-2 focus:outline-2 focus:outline focus:outline-blue-light rounded-lg text-blue-dark border-2 border-white" autoComplete="off" placeholder="Adres e-mail" type={'email'} id="email"/>
-            <label className="text-base text-white mb-1" htmlFor="message">Wiadomość</label>
-            <textarea 
-            className="mb-5 px-2 py-2 focus:outline-2 focus:outline focus:outline-blue-light rounded-lg text-blue-dark border-2 border-white" 
-            id="message" 
-            cols="30" 
-            placeholder="Wiadomość"
-            rows="10"></textarea>
-            <div className="w-full flex justify-center">
-              <button className="bg-blue-light px-6 xl:px-8 py-1.5 xl:py-2 rounded-full w-fit font-semibold text-white text-lg xl:text-xl ">Wyślij</button>
-            </div>
-          </form>
+          <ContactForm/>
         </div>
       </section>
       <FrequentlyAskedQuestions questions={questions}/>
